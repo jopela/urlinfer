071af77c1e067468caa19e8edfd8bd8199592024
Jonathan Pelletier <jonathan.pelletier1@gmail.com>
2013-12-04 15:58:55 +0000

    added test for wikivoyage

--- urlinfer.py
+++ urlinfer.py
@@ -22,18 +22,50 @@ def main():
             '--inferfunc',
             help='inference functions to apply',
             choices = ('wikivoyage', 'language'),
-            nargs='+',
-            required=True
+            nargs='+'
+            )
+
+    parser.add_argument(
+            '-t',
+            '--test',
+            help='run the test suit and exit',
+            action='store_true',
+            default=False
             )
 
     args = parser.parse_args()
 
+    if args.test:
+        import doctest
+        doctest.testmod()
+        exit(0)
+
     urls = args.urls.read().split()
     functions = args.inferfunc
 
+    return
 
-    print("urls: {0}".format(urls))
-    print("functions: {0}".format(functions))
+def wikivoyage(urls):
+    """ takes a list of urls and infers the wikivoyage urls from them.
+
+    Example
+    =======
+
+    # empty list should returnt the empty list.
+    >>> wikivoyage([])
+    []
+
+    # wikipedia urls should be 'casted' to wikivoyage urls.
+    >>> wikivoyage(['http://en.wikipedia.org/wiki/Montreal'])
+    ['http://en.wikipedia.org/wiki/Montreal','http://en.wikivoyage.org/wiki/Montreal']
+
+    # non wikipedia urls remain untouched.
+    >>> wikivoyage(['http://db.org/resource/Montreal'])
+    ['http://db.org/resource/Montreal']
+    """
+
+    return []
+
 
 if __name__ == '__main__':
     main()
